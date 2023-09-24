9c27485b1a4bfe6bc6741458db46a280aa4dbf14
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2023-09-24 13:10:21 -0500

    day 1 and two

--- day 1/main.rs
+++ day 1/main.rs
@@ -1,68 +1,74 @@
 // only time efficient solutions and their dependencies are in this file
 
-use std::io::*;
-use std::fs::File;
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::*;
 use std::process;
 
 fn main() {
-	puzzle_one();
-	puzzle_two();
+    puzzle_one();
+    puzzle_two();
 }
 
 fn puzzle_one() {
-	println!("\n Puzzle 1: \n");
+    println!("\n Puzzle 1: \n");
 
-	let mut set: HashSet<i32> = HashSet::new();
+    let mut set: HashSet<i32> = HashSet::new();
 
-	let input = File::open("./input.txt").unwrap();
-	let reader = BufReader::new(input);
+    let input = File::open("./input.txt").unwrap();
+    let reader = BufReader::new(input);
 
-	// I decided to use a set because it eliminates duplicate values and reduces the iteration and accessing times to roughly 0.
+    // I decided to use a set because it eliminates duplicate values and reduces
+    // the iteration and accessing times to roughly 0.
 
-	for line in reader.lines() {
-		let integer = line.unwrap().parse::<i32>().unwrap();
+    for line in reader.lines() {
+        let integer = line.unwrap().parse::<i32>().unwrap();
 
-		set.insert(integer);
-	}
+        set.insert(integer);
+    }
 
-	// iterate over all of the items in the set checking if the second value for the solution is in the set. If so, print and exit.
-	for number in &set {
-		let target: i32 = 2020 - number;
+    // iterate over all of the items in the set checking if the second value for
+    // the solution is in the set. If so, print and exit.
+    for number in &set {
+        let target: i32 = 2020 - number;
 
-		if set.contains(&target){
-			println!{"Values: {} and {}", number, target};
-			println!{"Answer: {}", number * target};
-			break;
-		}
-	}
+        if set.contains(&target) {
+            println! {"Values: {} and {}", number, target};
+            println! {"Answer: {}", number * target};
+            break;
+        }
+    }
 }
 
 fn puzzle_two() {
-	println!("\n Puzzle 2: \n");
+    println!("\n Puzzle 2: \n");
 
-	let mut set: HashSet<i32> = HashSet::new();
+    let mut set: HashSet<i32> = HashSet::new();
 
-	let input = File::open("./input.txt").unwrap();
-	let reader = BufReader::new(input);
+    let input = File::open("./input.txt").unwrap();
+    let reader = BufReader::new(input);
 
-	for line in reader.lines() {
-		let integer = line.unwrap().parse::<i32>().unwrap();
+    for line in reader.lines() {
+        let integer = line.unwrap().parse::<i32>().unwrap();
 
-		set.insert(integer);
-	}
+        set.insert(integer);
+    }
 
-	// While this implementation uses nested for loops, it is only O(n^2) in the worst case and is still the best solution to this problem. In terms of space complexity, it could be more efficient if I didn't copy all of the data into a set first, but I valued speed more than space in this instance.
-	
-	for number1 in &set {
-		for number2 in &set {
-			let target: i32 = 2020 - number1 - number2;
+    // While this implementation uses nested for loops, it is only O(n^2) in the
+    // worst case and is still the best solution to this problem.
+    // In terms of space complexity, it could be more efficient if I didn't copy
+    // all of the data into a set first, but I valued speed more than space in
+    // this instance.
 
-			if set.contains(&target){
-				println!{"Values: {}, {}, and {}", number1, number2, target};
-				println!{"Answer: {}", number1 * number2 * target};
-				process::exit(0x0100);
-			}
-		}
-	}
+    for number1 in &set {
+        for number2 in &set {
+            let target: i32 = 2020 - number1 - number2;
+
+            if set.contains(&target) {
+                println! {"Values: {}, {}, and {}", number1, number2, target};
+                println! {"Answer: {}", number1 * number2 * target};
+                process::exit(0x0100);
+            }
+        }
+    }
 }
---
+++ day 2/main.rs
@@ -0,0 +1,87 @@
+use std::fs::File;
+use std::io::*;
+
+fn main() {
+    puzzle_one();
+    puzzle_two();
+}
+
+fn count_characters(input: &str, character: &char) -> usize {
+    return input.matches(*character).count();
+}
+
+fn puzzle_one() {
+    println!("\n Puzzle 1: \n");
+
+    let mut valid_passwords_count: usize = 0;
+
+    let input = File::open("./input.txt").unwrap();
+    let reader = BufReader::new(input);
+
+    let mut range: &str;
+    let mut character_string: &str;
+    let mut password: &str;
+
+    for line in reader.lines() {
+        let line_value = line.unwrap();
+        let mut parts_iter = line_value.split_whitespace();
+
+        range = parts_iter.next().unwrap();
+        character_string = &parts_iter.next().unwrap()[0..1];
+        password = parts_iter.next().unwrap();
+
+        let character_array = character_string.chars().collect::<Vec<_>>();
+        let character = character_array.get(0).unwrap();
+
+        let num_chars: usize = count_characters(password, character);
+
+        let mut range_iter = range.split("-");
+
+        let range_min: usize =
+            range_iter.next().unwrap().parse::<usize>().unwrap();
+        let range_max: usize =
+            range_iter.next().unwrap().parse::<usize>().unwrap();
+
+        if range_min <= num_chars && num_chars <= range_max {
+            valid_passwords_count += 1;
+        }
+    }
+
+    println!("{}", valid_passwords_count);
+}
+
+fn puzzle_two() {
+    println!("\n Puzzle 2: \n");
+
+    let mut valid_passwords_count: usize = 0;
+
+    let input = File::open("./input.txt").unwrap();
+    let reader = BufReader::new(input);
+
+    for line in reader.lines() {
+        let line_value = line.unwrap();
+        let mut parts_iter = line_value.split_whitespace();
+
+        let range = parts_iter.next().unwrap();
+        let character_string = &parts_iter.next().unwrap()[0..1];
+        let password = parts_iter.next().unwrap();
+
+        let character_array = character_string.chars().collect::<Vec<_>>();
+        let character = character_array.get(0).unwrap();
+
+        let mut range_iter = range.split("-");
+
+        let pos_one: usize =
+            range_iter.next().unwrap().parse::<usize>().unwrap();
+        let pos_two: usize =
+            range_iter.next().unwrap().parse::<usize>().unwrap();
+
+        let pos_one_char = password.chars().nth(pos_one - 1).unwrap();
+        let pos_two_char = password.chars().nth(pos_two - 1).unwrap();
+
+        if (pos_one_char == *character) ^ (pos_two_char == *character) {
+            valid_passwords_count += 1;
+        }
+    }
+    println!("{}", valid_passwords_count);
+}
